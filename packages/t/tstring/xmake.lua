0bb209ec7fa1315090af545e83da8d8fefa110ff
Tab Ye <135727531+TabNahida@users.noreply.github.com>
2024-10-08 18:49:05 +0800

    Update xmake.lua

--- packages/t/tstring/xmake.lua
+++ packages/t/tstring/xmake.lua
@@ -4,7 +4,7 @@ package("tstring")
     set_description("TString is a custom C++ string implementation designed for efficient memory management.")
 
     add_urls("https://github.com/yourusername/TString/archive/refs/tags/$(version).tar.gz")
-    add_versions("v1.0-Delta", "c55552e7c8347cc179f54788a7a52f15554af8caeb925647a6e8dda7993939b0")
+    add_versions("v1.0-Epsilon", "cb3b6ed597422da2e5166ea4a388b631d4d640372c520d3f7e74a04d9b7235e5")
 
     add_includedirs("include", {public = true})
 
