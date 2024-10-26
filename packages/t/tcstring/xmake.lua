edcc9b6c7c3a475c18f0ac1f60f3393ef90fbc19
Tab Ye <135727531+TabNahida@users.noreply.github.com>
2024-10-26 15:45:53 +0800

    Create xmake.lua

---
+++ packages/t/tcstring/xmake.lua
@@ -0,0 +1,27 @@
+package("tcstring")
+    set_kind("library", {headeronly = true})
+    set_homepage("https://github.com/TabNahida/TCString")
+    set_description("TCString is a custom C string implementation designed for efficient memory management.")
+
+    add_urls("https://github.com/TabNahida/TCString/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/TabNahida/TCString.git")
+    add_versions("v0.1.0", "14d81d1bacbedf65423d84082fae7af6fc009d801a1c1e63ed5530d249de2015")
+
+    add_includedirs("include", {public = true})
+
+    on_install(function (package)
+        import("package.tools.xmake").install(package)
+    end)
+
+    on_test(function (package)
+        assert(package:has_cxxtypes("TCString", {includes = "TCString.h"}))
+        assert(package:check_cxxsnippets({
+            test = [[
+                #include <TCString.h>
+                void test() {
+                    TString str("Hello");
+                    str.append(", World!");
+                }
+            ]]
+        }, {configs = {languages = "c17"}}))
+    end)
